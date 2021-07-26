9f27e94f29cf340c18d11fbfd1b5bb4f3c8225f6
Atlas <jack@bentsen.us>
2021-07-26 10:29:16 -0700

    schema and seeds file

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS movie_reviews_db;
+
+CREATE movie_reviews_db;
+
+USE movie_reviews_db;
+
+CREATE TABLE movies (
+
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_name VARCHAR(100)
+
+);
+
+CREATE TABLE reviews (
+
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    review TEXT,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies(id)
+    ON DELETE SET NULL
+    
+);
---
+++ sql/seeds.sql
@@ -0,0 +1,16 @@
+USE movie_reviews_db;
+
+INSERT INTO movie (movie_name) 
+VALUES 
+('Space Jam: A New Legacy'),
+('The Forever Purge'),
+('Black Widow'),
+('The Boss Baby: Family Business'),
+('The Tomorrow War'),
+('Luca'),
+('Awake'),
+('Infinite'),
+('F9'),
+('Wrath of Man'),
+('Cruella'),
+('Peter Rabbit 2: The Runaway');
